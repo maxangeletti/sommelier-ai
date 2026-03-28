e1478e49fe7999b07cd6e9f5bcd775a8bf80f77d
Max <max.angeletti@gmail.com>
2026-03-28 19:55:01 +0100

    fix(ios): AppColors adattivo per Dark Mode
    - Usa UIColor.systemBackground invece di colori statici
    - Risolve testo invisibile in Dark Mode

--- ios-app/SommelierAI/SommelierAI/AppColors.swift
+++ ios-app/SommelierAI/SommelierAI/AppColors.swift
@@ -5,7 +5,6 @@
 //  Created by Massimiliano Angeletti on 27/02/26.
 //
 
-
 import SwiftUI
 
 enum AppColors {
@@ -13,9 +12,8 @@ enum AppColors {
     static let primaryWine = Color(red: 139/255, green: 30/255, blue: 45/255)
     static let accentWine  = Color(red: 198/255, green: 40/255, blue: 40/255)
 
-    static let backgroundPrimary = Color(red: 246/255, green: 242/255, blue: 241/255)
-    static let backgroundSecondary = Color(red: 236/255, green: 231/255, blue: 230/255)
-
-    static let cardBackground = Color.white
-
+    // Adattivi a Dark Mode
+    static let backgroundPrimary = Color(UIColor.systemBackground)
+    static let backgroundSecondary = Color(UIColor.secondarySystemBackground)
+    static let cardBackground = Color(UIColor.secondarySystemBackground)
 }
